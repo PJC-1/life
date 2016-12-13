6bd916dda7ee95b841480c6cd6c12cc66dbaa5e8
PJC-1 <jumpzine@gmail.com>
2016-12-13 09:23:50 -0800

    commented out the buy function for now and reorganized the appjs and added comments

--- script/app.js
+++ script/app.js
@@ -1,20 +1,25 @@
+// cached strings
 var moneySign = "$ ";
 var purchasedSign = "Purchased: ";
 var hungerSign = "Hunger: ";
 var hungryNoMoney = "You are hungry, but don't have enough money for this item.";
 var notHungry = "You are not hungry now.";
 
-
+// user object
 var userObj = {
     money: 0,
-    hunger: 90,
+    hunger: 75,
     happiness: 100,
     purchased: []
 }
 
-var time = setInterval(addMoneyByTime, 1500);
+// broadcast
+function clearBroadcast(){
+  document.getElementById('broadcast').innerHTML = "";
+}
 
-var hunger = setInterval(gettingHungery, 120000);
+// money
+var time = setInterval(addMoneyByTime, 1500);
 
 function funTime(){
   userObj.money += 5;
@@ -26,9 +31,8 @@ function addMoneyByTime(){
   document.getElementById('money').innerHTML = moneySign + userObj.money;
 }
 
-function clearBroadcast(){
-  document.getElementById('broadcast').innerHTML = "";
-}
+// hunger
+var hunger = setInterval(gettingHungery, 120000);
 
 function ramenSnack(){
     if (userObj.hunger <= 95 && userObj.money > 5){
@@ -51,8 +55,9 @@ function gettingHungery(){
   document.getElementById('userHunger').innerHTML = hungerSign + userObj.hunger;
 }
 
-function buy(){
-  userObj.money -= 100;
-  userObj.purchased.push("example string");
-  document.getElementById('userPurchased').innerHTML = purchasedSign + userObj.purchased;
-}
+// shopping
+// function buy(){
+//   userObj.money -= 100;
+//   userObj.purchased.push("example string");
+//   document.getElementById('userPurchased').innerHTML = purchasedSign + userObj.purchased;
+// }
