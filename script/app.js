5b4e06fc9457314a83ed8cff9f3b7cebecbebfa0
PJC-1 <jumpzine@gmail.com>
2016-12-13 10:26:10 -0800

    changed the work image and added some other images and chaged the rates of hunger and earning money

--- script/app.js
+++ script/app.js
@@ -9,61 +9,53 @@ var notHungry = "You are not hungry now.";
 var userObj = {
     money: 1.00,
     hunger: 75,
-    happiness: 75,
-    purchased: []
+    happiness: 75
 }
 
 // broadcast
 function clearBroadcast(){
-  document.getElementById('broadcast').innerHTML = "";
+    document.getElementById('broadcast').innerHTML = "";
 }
 
 // money
 var time = setInterval(addMoneyByTime, 1700);
 
 function funTime(){
-  userObj.money += 1.00;
-  document.getElementById('money').innerHTML = moneySign + userObj.money;
+    userObj.money += 1.00;
+    document.getElementById('money').innerHTML = moneySign + userObj.money;
 }
 
 function addMoneyByTime(){
-  userObj.money += 0.25;
-  document.getElementById('money').innerHTML = moneySign + userObj.money;
+    userObj.money += 0.25;
+    document.getElementById('money').innerHTML = moneySign + userObj.money;
 }
 
 if (userObj.money < 1){
-  alert("You officially broke");
-  userObj.money = 0;
-  userObj.happiness -= 25;
+    alert("You officially broke");
+    userObj.money = 0;
+    userObj.happiness -= 25;
 }
 
 // hunger
-var hunger = setInterval(gettingHungery, 120000);
-
-function ramenSnack(){
-    if (userObj.hunger <= 95 && userObj.money > 5){
-      userObj.hunger += 10;
-      userObj.money -= 5;
-      document.getElementById('userHunger').innerHTML = hungerSign + userObj.hunger;
-    } else if (userObj.hunger < 95) {
-      document.getElementById('broadcast').innerHTML = hungryNoMoney;
-      setTimeout(clearBroadcast, 3000);
-    } else if (userObj.hunger > 95) {
-      document.getElementById('broadcast').innerHTML = notHungry;
-      setTimeout(clearBroadcast, 3000);
-    } else if (userObj.hunger < 95 && userObj.money < 5){
-      alert("you are hungry and without money.");
-    }
-}
+var hunger = setInterval(gettingHungery, 30000);
 
 function gettingHungery(){
-  userObj.hunger -= 1;
-  document.getElementById('userHunger').innerHTML = hungerSign + userObj.hunger;
+    userObj.hunger -= 1;
+    document.getElementById('userHunger').innerHTML = hungerSign + userObj.hunger;
 }
 
-// shopping
-// function buy(){
-//   userObj.money -= 100;
-//   userObj.purchased.push("example string");
-//   document.getElementById('userPurchased').innerHTML = purchasedSign + userObj.purchased;
-// }
+function ramenSnack(){
+    if (userObj.hunger <= 95 && userObj.money > 2.25){
+        userObj.hunger += 1;
+        userObj.money -= 2.25;
+        document.getElementById('userHunger').innerHTML = hungerSign + userObj.hunger;
+    } else if (userObj.hunger < 95) {
+        document.getElementById('broadcast').innerHTML = hungryNoMoney;
+        setTimeout(clearBroadcast, 3000);
+    } else if (userObj.hunger > 95) {
+        document.getElementById('broadcast').innerHTML = notHungry;
+        setTimeout(clearBroadcast, 3000);
+    } else if (userObj.hunger < 95 && userObj.money < 2.25){
+        alert("you are hungry and without money.");
+    }
+}
