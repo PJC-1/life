09c71411f38c1457d76de065fa26e81e21b7fb58
PJC-1 <jumpzine@gmail.com>
2016-12-13 09:11:49 -0800

    added more to the userObj and added some hunger eating functionality

--- script/app.js
+++ script/app.js
@@ -1,21 +1,54 @@
 var moneySign = "$ ";
 var purchasedSign = "Purchased: ";
+var hungerSign = "Hunger: ";
+var hungryNoMoney = "You are hungry, but don't have enough money for this item.";
+var notHungry = "You are not hungry now.";
+
 
 var userObj = {
     money: 0,
+    hunger: 90,
+    happiness: 100,
     purchased: []
 }
 
-var time = setInterval(addMoneyByTime, 1000);
+var time = setInterval(addMoneyByTime, 1500);
+
+var hunger = setInterval(gettingHungery, 120000);
 
 function funTime(){
-  userObj.money += 5
+  userObj.money += 5;
   document.getElementById('money').innerHTML = moneySign + userObj.money;
 }
 
 function addMoneyByTime(){
   userObj.money += 1;
   document.getElementById('money').innerHTML = moneySign + userObj.money;
+}
+
+function clearBroadcast(){
+  document.getElementById('broadcast').innerHTML = "";
+}
+
+function ramenSnack(){
+    if (userObj.hunger <= 95 && userObj.money > 5){
+      userObj.hunger += 10;
+      userObj.money -= 5;
+      document.getElementById('userHunger').innerHTML = hungerSign + userObj.hunger;
+    } else if (userObj.hunger < 95) {
+      document.getElementById('broadcast').innerHTML = hungryNoMoney;
+      setTimeout(clearBroadcast, 3000);
+    } else if (userObj.hunger >= 100) {
+      document.getElementById('broadcast').innerHTML = notHungry;
+      setTimeout(clearBroadcast, 3000);
+    } else if (userObj.hunger < 95 && userObj.money < 5){
+      alert("you are hungry and without money.");
+    }
+}
+
+function gettingHungery(){
+  userObj.hunger -= 1;
+  document.getElementById('userHunger').innerHTML = hungerSign + userObj.hunger;
 }
 
 function buy(){
