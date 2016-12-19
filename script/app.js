289f7b58552deb049b96101cce2a82c913b236af
PJC-1 <jumpzine@gmail.com>
2016-12-19 10:28:05 -0800

    switched out to using the food object instead of hardcording the image sources

--- script/app.js
+++ script/app.js
@@ -165,9 +165,12 @@ function pizzaSnack(){
 var lightSwtich = true;
 
 function testAdding(){
+    var src = foodObj.pizzaSnack.src;
     if(userObj.money > 5 && lightSwtich === true) {
         var i = document.createElement("IMG");
-        i.setAttribute("src", "./assets/pizzaRolls.png");
+        // i.setAttribute("src", "./assets/pizzaRolls.png");
+        i.setAttribute("src", src);
+
         i.setAttribute("width", "50");
         i.setAttribute("onclick", "pizzaSnack()");
         i.setAttribute("id", "pizzaPizza");
